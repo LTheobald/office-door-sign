18aef1cdf3ad00609901af341ee9eedb910b302e
Lee Theobald <lee@ltheobald.co.uk>
2021-01-04 10:50:48 +0000

    Tidy ups and comments

--- project/app.py
+++ project/app.py
@@ -15,6 +15,7 @@ current_status = Status.FREE
 
 
 def set_status(status):
+    """Set the current status to one matching the passed in string if appropriate"""
     global current_status
     for name, member in Status.__members__.items():
         if (name == status) | (name.lower() == status):
@@ -26,17 +27,20 @@ def set_status(status):
 
 
 def draw():
+    """Updates the UnicornHatMini to reflect current status"""
     unicornhatmini.set_all(current_status.r, current_status.g, current_status.b)
     unicornhatmini.show()
 
 
 @app.route("/")
 def root():
+    """A simple message to prove the API is up and accepting requests"""
     return jsonify({"running":True}), 200
 
 
 @app.route("/switch")
 def switchOnOff():
+    """An easy switch to turn the display on or off"""
     global current_status
     if current_status is Status.OFF:
         current_status = Status.FREE
@@ -48,10 +52,12 @@ def switchOnOff():
 
 @app.route("/status/<status>")
 def status(status):
+    """Change the status to the supplied string"""
     safeStatus = escape(status)
     if set_status(safeStatus):
         return jsonify({"status": safeStatus}), 200
     return jsonify({"status": "UNKNOWN: " + safeStatus}), 404
 
 
+# LET'S DO THIS!
 draw()
