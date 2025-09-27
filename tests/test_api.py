79c14aa8faa3f9f5b393349cc1562fd9daaeeb44
Lee Theobald <lee@ltheobald.co.uk>
2025-09-27 18:56:08 +0100

    Changing tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,8 +2,8 @@
 
 from fastapi.testclient import TestClient
 
-from office_sign.api import create_app
-from office_sign.hardware.mock import MockLightPanel
+from src.office_sign.api import create_app
+from src.office_sign.hardware.mock import MockLightPanel
 
 
 def test_health_check() -> None:
@@ -13,20 +13,34 @@ def test_health_check() -> None:
   assert response.json() == {"status": "ok"}
 
 
-def test_turn_light_on_and_off() -> None:
+def test_all_statuses() -> None:
   panel = MockLightPanel()
   client = TestClient(create_app(panel))
 
-  turn_on = client.post("/light/on")
-  assert turn_on.status_code == 200
-  assert turn_on.json() == {"power_on": True}
-  assert panel.power_on is True
+  status_client = client.post("/status/free")
+  assert status_client .status_code == 200
+  assert status_client .json() == {"status": "FREE"}
+  assert panel.status.name is "FREE"
+  assert panel.status.color is (0, 255, 0)
 
-  turn_off = client.post("/light/off")
-  assert turn_off.status_code == 200
-  assert turn_off.json() == {"power_on": False}
-  assert panel.power_on is False
+  status_client = client.post("/status/working")
+  assert status_client .status_code == 200
+  assert status_client .json() == {"status": "WORKING"}
+  assert panel.status.name is "WORKING"
+  assert panel.status.color is (255, 255, 0)
 
-  status = client.get("/light/status")
-  assert status.status_code == 200
-  assert status.json() == {"power_on": False}
+  status_client = client.post("/status/on_call")
+  assert status_client .status_code == 200
+  assert status_client .json() == {"status": "ON_CALL"}
+  assert panel.status.name is "ON_CALL"
+  assert panel.status.color is (255, 0, 0)
+
+  status_client = client.post("/status/off")
+  assert status_client .status_code == 200
+  assert status_client .json() == {"status": "OFF"}
+  assert panel.status.name is "OFF"
+  assert panel.status.color is (0, 0, 0)
+
+  status_client = client.post("/status/invalid")
+  assert status_client .status_code == 404
+
