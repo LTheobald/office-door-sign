deec523abae5d406aaf92b32583c3548cda95124
Lee Theobald <lee@ltheobald.co.uk>
2025-09-27 19:33:00 +0100

    Attempt to fix code formatting issues

--- src/office_sign/main.py
+++ src/office_sign/main.py
@@ -9,7 +9,4 @@ from .api import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    uvicorn.run("office_sign.main:app",
-                host="0.0.0.0",
-                port=8000,
-                reload=False)
+    uvicorn.run("office_sign.main:app", host="0.0.0.0", port=8000, reload=False)
